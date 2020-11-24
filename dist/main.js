e04d86a1996a3e96d4c77c7fdfd81234a3d7fffd
Natalia <nataliostapiuk@gmail.com>
2020-11-24 13:42:16 +0200

    finished

--- dist/main.js
+++ dist/main.js
@@ -1,21 +1,19 @@
 const render = new Renderer()
 
-
-
 const getReceipes = function () {
     let input = $('#input').val()
     $.get(`/recipes/${input}`, function (data) {
-      //  console.log(data);
         render.renderData(data)
     })
 
 }
 
-Handlebars.registerHelper('clickHelper', function() {
-	return JSON.stringify(player);
+Handlebars.registerHelper('clickHelper', function(food) {
+	return JSON.stringify(food);
 });
 
-const onPlayerClick = player => {
-	console.log(player);
+const onFoodClick = food => {
+    let firstIngredient = food.ingredients[0]
+    alert(firstIngredient)
 };
 
--- server.js
+++ server.js
@@ -8,8 +8,6 @@ app.use(express.static(path.join(__dirname, 'node_modules')))
 
 app.get('/recipes/:ingredient', function (req, res) {
     let ingredient = req.params.ingredient
-    console.log('ok');
-
     urllib.request(`https://recipes-goodness.herokuapp.com/recipes/${ingredient}`, function (err, response) {
         data = JSON.parse(response.toString())
         console.log(data);
@@ -18,9 +16,7 @@ app.get('/recipes/:ingredient', function (req, res) {
            title: r.title,
            img: r.thumbnail,
            href: r.href
-
        } })
-       console.log(recipesList);
        res.send(recipesList)
     })
 })
