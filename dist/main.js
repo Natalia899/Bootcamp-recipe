da29a718db53b6ddd18428e37ea6ff101238ef46
Natalia <nataliostapiuk@gmail.com>
2020-11-24 13:20:45 +0200

    finished

--- dist/main.js
+++ dist/main.js
@@ -5,8 +5,17 @@ const render = new Renderer()
 const getReceipes = function () {
     let input = $('#input').val()
     $.get(`/recipes/${input}`, function (data) {
-        console.log(data);
+      //  console.log(data);
         render.renderData(data)
     })
 
 }
+
+Handlebars.registerHelper('clickHelper', function() {
+	return JSON.stringify(player);
+});
+
+const onPlayerClick = player => {
+	console.log(player);
+};
+
